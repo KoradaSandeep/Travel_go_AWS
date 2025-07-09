868c53b2ef3705a53c102e58782e21688cd69dd9
sandeep <koradasandeep123@gmail.com>
2025-07-09 18:56:25 +0530

    add contents

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from datetime import datetime
-import os
 import boto3
 import uuid # For generating unique booking IDs
 from botocore.exceptions import ClientError
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = 'f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a'
 
 # AWS Configuration (replace with your actual region and SNS Topic ARN)
 AWS_REGION = 'ap-south-1'  # Example: Mumbai region
@@ -126,7 +125,6 @@ def home():
         return redirect(url_for('login'))
     current_user_phone = session['user']
     user_bookings = get_user_bookings(current_user_phone)
-    # Convert ISO format string back to datetime object for rendering
     for booking in user_bookings:
         if 'booked_at' in booking:
             try:
@@ -369,4 +367,4 @@ def others():
     return render_template('others.html')
 
 if __name__ == '__main__':
-    app.run(debug=True , port=5000)
+    app.run(debug=True)
